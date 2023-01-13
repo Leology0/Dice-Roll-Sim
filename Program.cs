9070c365b2a902a8de6a4b898e9d061b0ef6addf
Leology0 <leology42@gmail.com>
2023-01-13 12:55:13 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -27,12 +27,14 @@ while (loop)
 
     // Option 1
     if (chooseNumber == "1") {
+        Console.WriteLine("\nRoll Dice Once");
         int dice1 = (rnd.Next(1,6));
         int dice2 = (rnd.Next(1,6));
         Console.WriteLine($"{dice1},{dice2} (sum: {dice1 + dice2})");
     }
     // Option 2
     if (chooseNumber == "2") {
+        Console.WriteLine("\nRoll Dice 5 Times");
         for (int n = 1; n <= 5; n++) {
             int dice1 = (rnd.Next(1,6));
             int dice2 = (rnd.Next(1,6));
@@ -41,8 +43,29 @@ while (loop)
     }
     // Option 3
     if (chooseNumber == "3") {
+        Console.WriteLine("\n Roll Dice 'n' Times");
         Console.Write("How many times would you like to roll:");
-        int n = Convert.ToInt32(Console.ReadLine());
+        int rollAmount = Convert.ToInt32(Console.ReadLine());
+        for (int n = 1; n <= rollAmount; n++) {
+            int dice1 = (rnd.Next(1,6));
+            int dice2 = (rnd.Next(1,6));
+            Console.WriteLine($"{dice1},{dice2} (sum: {dice1 + dice2})");
+        }
+    }
+
+    // Option 4
+    if (chooseNumber == "4") {
+        Console.WriteLine("\nRoll Dice Until Snake Eyes");
+        int rollCount = 0;
+        int dice1 = 0;
+        int dice2 = 0;
+        while (dice1 != 1 || dice2 != 1) {
+            dice1 = (rnd.Next(1,6));
+            dice2 = (rnd.Next(1,6));
+            Console.WriteLine($"{dice1},{dice2} (sum: {dice1 + dice2})");
+            rollCount++;
+        }
+        Console.WriteLine($"SNAKE EYES! It took {rollCount} rolls to get snake eyes");
     }
 
     // End Loop
@@ -51,3 +74,4 @@ while (loop)
     }
 
 }
+Console.WriteLine("\nGoodbye");
